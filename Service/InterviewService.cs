61c8bbe25622b0b1021fbeec0ff2c53efcf4c377
Mootaz Abdellatif <mootaz.abdellatif@esprit.tn>
2019-10-29 01:59:56 +0100

    Initial Commit

---
+++ Service/InterviewService.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using Service.Pattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class InterviewService : Service<Interview>, IInterviewService
+    {
+        static IDataBaseFactory Factory = new DataBaseFactory();
+        static IUnitOfWork Uok = new UnitOfWork(Factory);
+        public InterviewService() : base(Uok)
+        {
+        }
+
+        //récupérer les chambre d'un hotel
+        public IEnumerable<Interview> GetInterviewByUser(int userId)
+        {
+            return null;
+            //return GetMany(ch => ch.Category.Equals(nameCategory));
+        }
+
+    }
+}
